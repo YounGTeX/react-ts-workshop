3ea04545d1aea86b4122d5b5d1598c9cd6f93acb
YounGTeX <thiagoextremer@gmail.com>
2022-01-04 19:32:11 -0300

    register inputs

--- src/pages/Auth/Register/index.tsx
+++ src/pages/Auth/Register/index.tsx
@@ -2,9 +2,24 @@ import React from 'react';
 import { Button, Typography } from '@material-ui/core';
 import { NavLink } from 'react-router-dom';
 import { useStyles } from './styles';
+import { FormProvider, useForm } from 'react-hook-form';
+import Input from '../../../components/Input';
+
+interface SignUpProps {
+  email: string;
+  name: string;
+  password: string;
+  confirmPassword: string;
+}
 
 const Register = (): JSX.Element => {
   const classes = useStyles();
+  const formMethods = useForm<SignUpProps>();
+  const { handleSubmit } = formMethods;
+
+  const onSubmit = () => {
+    console.log('Submit');
+  };
 
   return (
     <div className={classes.container}>
@@ -14,7 +29,17 @@ const Register = (): JSX.Element => {
             Insira suas informações para o registro
           </Typography>
         </div>
-        {/* to do form*/}
+        <FormProvider {...formMethods}>
+          <Input variant='outlined' name='email' label='E-mail' required givenError='Insira seu nome' />
+          <Input variant='outlined' name='password' label='Senha' required givenError='Insira sua senha' />
+          <Input
+            variant='outlined'
+            name='confirmPassword'
+            label='Confirme sua senha'
+            required
+            givenError='Insira sua confirmação de senha'
+          />
+        </FormProvider>
         <div className={classes.bottomContainer}>
           <Button className={classes.button} type='submit'>
             Cadastrar
