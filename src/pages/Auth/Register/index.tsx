b47f79e4ae0528683fce0b00f7c4014753e9e453
YounGTeX <thiagoextremer@gmail.com>
2022-01-05 18:38:53 -0300

    Register

--- src/pages/Auth/Register/index.tsx
+++ src/pages/Auth/Register/index.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Typography } from '@material-ui/core';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { useStyles } from './styles';
 import { FormProvider, useForm } from 'react-hook-form';
 import Input from '../../../components/Input';
+import { useAuth } from '../../../hooks/Auth';
+import { showAlert } from '../../../utils/showAlert';
+import Loading from '../../../components/Loading';
 
 interface SignUpProps {
   email: string;
@@ -16,33 +19,84 @@ const Register = (): JSX.Element => {
   const classes = useStyles();
   const formMethods = useForm<SignUpProps>();
   const { handleSubmit } = formMethods;
+  const { signUp } = useAuth();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState<boolean>(false);
 
-  const onSubmit = () => {
-    console.log('Submit');
+  const onSubmit = async (data: SignUpProps): Promise<void> => {
+    const { confirmPassword, ...userData } = data;
+
+    if (confirmPassword !== userData.password) {
+      showAlert({
+        title: 'Atenção!',
+        icon: 'warning',
+        text: 'As senhas precisam ser iguais!',
+      });
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const res = await signUp(userData);
+
+      showAlert({
+        title: 'Sucesso!',
+        icon: 'success',
+        text: res.message,
+      }).then(result => {
+        if (result.isConfirmed) {
+          navigate('/auth/login', { state: { email: res.content?.email } });
+        }
+      });
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+      showAlert({
+        title: 'Ops...',
+        icon: 'error',
+        text: err.response.data.message,
+      });
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <div className={classes.container}>
-      <form className={classes.formContainer}>
+      <form className={classes.formContainer} onSubmit={handleSubmit(onSubmit)}>
         <div className={classes.header}>
           <Typography color='secondary' variant='h5'>
             Insira suas informações para o registro
           </Typography>
         </div>
         <FormProvider {...formMethods}>
-          <Input variant='outlined' name='email' label='E-mail' required givenError='Insira seu nome' />
-          <Input variant='outlined' name='password' label='Senha' required givenError='Insira sua senha' />
+          <Input
+            variant='outlined'
+            name='email'
+            label='E-mail'
+            required
+            givenError='Insira um endereço de e-mail'
+          />
+          <Input variant='outlined' name='name' label='Nome' required givenError='Insira seu nome' />
+          <Input
+            variant='outlined'
+            name='password'
+            label='Senha'
+            type='password'
+            required
+            givenError='Insira uma senha'
+          />
           <Input
             variant='outlined'
             name='confirmPassword'
-            label='Confirme sua senha'
+            label='Confirme a senha'
+            type='password'
             required
-            givenError='Insira sua confirmação de senha'
+            givenError='Insira a senha novamente'
           />
         </FormProvider>
         <div className={classes.bottomContainer}>
           <Button className={classes.button} type='submit'>
-            Cadastrar
+            {loading ? <Loading loadingSize={16} /> : 'Cadastrar'}
           </Button>
           <NavLink className={classes.signIn} to='/auth/login'>
             <Typography variant='h6'>Já possuo uma conta</Typography>
