e4d08ffcd557bb6c36b548ab351d486729089161
YounGTeX <thiagoextremer@gmail.com>
2022-01-05 18:22:39 -0300

    Auth pronto

--- src/hooks/Auth/index.tsx
+++ src/hooks/Auth/index.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, useCallback, useContext, useState } from 'react';
 import { User } from '../../@types/User';
 import { Response } from '../../@types/Response';
-import { api } from '../../services';
+import { api } from '../../services/api';
 
 interface AuthCredentials {
   email: string;
@@ -28,17 +28,52 @@ interface AuthContextData {
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
-  const [user, setUser] = useState<User>({} as User);
+  const [user, setUser] = useState<User>(() => {
+    const storagedUser = localStorage.getItem('@krud:user');
+    const storagedToken = localStorage.getItem('@krud:token');
 
-  const signIn = useCallback(async () => { }, []);
-  const signUp = useCallback(async () => { }, []);
+    if (storagedUser && storagedToken) {
+      api.defaults.headers.common['authorization'] = `Bearer ${storagedToken}`;
+      return JSON.parse(storagedUser);
+    }
+    return {} as User;
+  });
 
-  return <AuthContext.Provider value={{ user, signIn, signUp }}>{children}</AuthContext.Provider>;
+  const signIn = useCallback(async (credentials: AuthCredentials) => {
+    const { data }: { data: Response<LoginResponse> } = await api.post('auth/login', credentials);
+
+    localStorage.setItem('@krud:user', JSON.stringify(data.content?.user));
+    localStorage.setItem('@krud:token', data.content?.token as string);
+
+    api.defaults.headers.common['authorization'] = `Bearer ${data.content?.token}`;
+    setUser(data.content?.user as User);
+  }, []);
+
+  const signUp = useCallback(async (userData: SignUpData): Promise<Response<User>> => {
+    const { data }: { data: Response<User> } = await api.post('users/create', userData);
+
+    return data;
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        signIn,
+        signUp,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = (): AuthContextData => {
   const context = useContext(AuthContext);
+
   if (!context) {
     throw new Error('Este hook deve ser utilizado dentro de seu provider');
   }
+
+  return context;
 };
